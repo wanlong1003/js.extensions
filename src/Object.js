25b76ba79e296da5358f30a6387d5117564565f2
Wanlong Liu <wanlong1003@163.com>
2018-11-28 14:53:48 +0800

    添加了对象合并的方法

--- src/Object.js
+++ src/Object.js
@@ -85,6 +85,21 @@
             value: function () {
                 return JSON.stringify(this);
             }
+        },
+        Extend: {
+            writable: true,
+            enumerable: false,
+            configurable: true,
+            value: function (o) {
+                var names = Object.getOwnPropertyNames(o);
+                for (var i = 0; i < names.length; i++) {
+                    if (names[i] in this) {
+                        continue;
+                    }
+                    var desc = Object.getOwnPropertyDescriptor(o, names[i]);
+                    Object.defineProperty(this, names[i], desc);
+                }
+            }
         }
     });
 })(window.Object);
